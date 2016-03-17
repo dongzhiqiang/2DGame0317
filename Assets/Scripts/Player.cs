e44e625ab61c955e2183c1ad9ff5f181288a351d
dongzhiqiang <d_zhi_q@163.com>
2016-03-17 09:30:36 +0800

    首次提交

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,107 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+
+    private Rigidbody2D myRigidbody;
+
+    private Animator myAnimator;
+
+    [SerializeField]
+    private float movementSpeed;
+
+    private bool attack;
+
+    private bool facingRight;
+
+    private bool slider;
+
+    // Use this for initialization
+    void Start()
+    {
+        facingRight = true;
+        myRigidbody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        HandleInput();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+
+        HandleMovement(horizontal);
+        Flip(horizontal);
+        HandleAttacks();
+
+        ResetValues();
+    }
+
+    void HandleMovement(float horizontal)
+    {
+        if (!myAnimator.GetBool("slide") && !this.myAnimator.GetCurrentAnimatorStateInfo(0).IsTag("Attack"))
+        {
+            myRigidbody.velocity = new Vector2(horizontal * movementSpeed, myRigidbody.velocity.y);
+        }
+
+        if (slider && !this.myAnimator.GetCurrentAnimatorStateInfo(0).IsName("Slide"))
+        {
+            myAnimator.SetBool("slide", true);
+        }
+        else if (!this.myAnimator.GetCurrentAnimatorStateInfo(0).IsName("Slide"))
+        {
+            myAnimator.SetBool("slide", false);
+        }
+
+
+        myAnimator.SetFloat("speed", Mathf.Abs(horizontal));
+    }
+
+    void HandleAttacks()
+    {
+        if (attack && !this.myAnimator.GetCurrentAnimatorStateInfo(0).IsTag("Attack"))
+        {
+            myAnimator.SetTrigger("attack");
+            myRigidbody.velocity = Vector2.zero;
+        }
+    }
+
+    void HandleInput()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            attack = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            slider = true;
+        }
+    }
+
+    void Flip(float horizontal)
+    {
+        if (horizontal > 0 && !facingRight || horizontal < 0 && facingRight)
+        {
+            facingRight = !facingRight;
+
+            Vector3 theScale = transform.localScale;
+
+            theScale.x *= -1;
+
+            transform.localScale = theScale;
+        }
+
+    }
+
+    void ResetValues()
+    {
+        attack = false;
+        slider = false;
+    }
+}
